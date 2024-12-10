91ad2f36f67fcc6b9119f686e6cadd054eb6b008
Ralph Bauer <200582@fhwn.ac.at>
2024-12-10 01:45:27 +0100

    refactor notes router

--- backend/src/app/routers/notes.py
+++ backend/src/app/routers/notes.py
@@ -1,84 +1,32 @@
 import datetime
 from typing import List
-
 from fastapi import APIRouter, Depends
-
 from .. import schemas, dependencies, usecases
+from sqlalchemy.orm import Session
+from ..database import get_db
 
 router = APIRouter(
     prefix='/notes',
     tags=['notes'],
 )
 
-
-@router.get("/",
-                response_model=List[schemas.Note],
-                operation_id='readNotes',
-                description='''
-                    Return all note of an existing session.
-                ''',
-                responses={
-                },
-            )
-def read_notes(token: str = Depends(dependencies.get_session_token)):
-    notes = usecases.notes_crud.read_notes(token)
-
+@router.get("/", response_model=List[schemas.Note])
+def read_notes(token: str = Depends(dependencies.get_session_token), db: Session = Depends(get_db)):
+    notes = usecases.notes_crud.read_notes(db, token)
     return notes
 
+@router.post("/", response_model=schemas.Note)
+def create_note(note_data: schemas.NoteCreate, token: str = Depends(dependencies.get_session_token), db: Session = Depends(get_db)):
+    return usecases.notes_crud.create_note(db, token, note_data.dict())
 
-@router.post("/",
-             response_model=schemas.Note,
-             operation_id='createNote',
-             description='''
-                 Create a new note for the session.
-             ''',
-             responses={
-                 201: {"description": "Note created successfully"},
-                 400: {"description": "Invalid input"},
-             },
-             )
-def create_note(note_data: schemas.note.NoteCreate, token: str = Depends(dependencies.get_session_token)):
-    return usecases.notes_crud.create_note(token, note_data.dict())
+@router.get("/{note_id}", response_model=schemas.Note)
+def read_note(note_id: int, token: str = Depends(dependencies.get_session_token), db: Session = Depends(get_db)):
+    return usecases.notes_crud.read_note(db, token, note_id)
 
+@router.put("/{note_id}", response_model=schemas.Note)
+def update_note(note_id: int, note_data: schemas.NoteUpdate, token: str = Depends(dependencies.get_session_token), db: Session = Depends(get_db)):
+    return usecases.notes_crud.update_note(db, token, note_id, note_data.dict())
 
-@router.get("/{note_id}",
-            response_model=schemas.Note,
-            operation_id='readNote',
-            description='''
-                Return a specific note by ID.
-            ''',
-            responses={
-                404: {"description": "Note not found"},
-            },
-            )
-def read_note(note_id: int, token: str = Depends(dependencies.get_session_token)):
-    return usecases.notes_crud.read_note(token, note_id)
-
-
-@router.put("/{note_id}",
-            response_model=schemas.Note,
-            operation_id='updateNote',
-            description='''
-                Update a specific note by ID.
-            ''',
-            responses={
-                404: {"description": "Note not found"},
-                400: {"description": "Invalid input"},
-            },
-            )
-def update_note(note_id: int, note_data: schemas.note.NoteUpdate, token: str = Depends(dependencies.get_session_token)):
-    return usecases.notes_crud.update_note(token, note_id, note_data.dict())
-
-
-@router.delete("/{note_id}",
-               response_model=dict,
-               operation_id='deleteNote',
-               description='''
-                   Delete a specific note by ID.
-               ''',
-               responses={
-                   404: {"description": "Note not found"},
-               },
-               )
-def delete_note(note_id: int, token: str = Depends(dependencies.get_session_token)):
-    return usecases.notes_crud.delete_note(token, note_id)
+@router.delete("/{note_id}", response_model=dict)
+def delete_note(note_id: int, token: str = Depends(dependencies.get_session_token), db: Session = Depends(get_db)):
+    return usecases.notes_crud.delete_note(db, token, note_id)

d44d39e73b1b63d5a64153c1ebe49bd0eaff5e23
Ralph Bauer <200582@fhwn.ac.at>
2024-12-10 01:48:23 +0100

    add FarmNote_Label

---
+++ frontend/src/components/FarmNoteLabel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="farmnote-label">
+    FARM NOTE
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FarmNoteLabel"
+}
+</script>
+
+<style scoped>
+.farmnote-label {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: white;
+  background-color: #28a745;
+  padding: 20px;
+  width: 100%;
+  text-align: center;
+}
+</style>

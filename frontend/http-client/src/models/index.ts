6e12fa218248d52fcfa228efab3b4630533bb012
Ralph Bauer <200582@fhwn.ac.at>
2024-12-10 01:51:10 +0100

    add http client stuff

---
+++ frontend/http-client/src/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './HTTPError';
+export * from './HTTPValidationError';
+export * from './Note';
+export * from './NoteCreate';
+export * from './NoteUpdate';
+export * from './Session';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';

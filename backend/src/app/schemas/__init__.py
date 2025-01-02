da050ab0a65381b6579463a8ad32e934286f6abd
Ralph Bauer <200582@fhwn.ac.at>
2025-01-02 14:26:03 +0100

    add imports

--- backend/src/app/schemas/__init__.py
+++ backend/src/app/schemas/__init__.py
@@ -2,4 +2,4 @@
 The schema package contains the api models.
 They are used as DTOs (Data-Transfer-Objects) and to validate the given input
 """
-from .note import Note
+from .note import Note, NoteCreate, NoteUpdate, NoteBase
